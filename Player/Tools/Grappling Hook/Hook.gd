678412a53955bbf171b8a57134d89d887fcbe98d
CaptainAdmiral <mattb1197@live.co.uk>
2020-06-27 00:53:42 +0100

    Copy paste heresy

--- Player/Tools/Grappling Hook/Hook.gd
+++ Player/Tools/Grappling Hook/Hook.gd
@@ -1,23 +1,20 @@
-extends Area2D #TODO change to kinematic
+extends Area2D
 
 var shooter
 var motion = Vector2()
 var isAttached = false
 var length = 0
 var attatchedTo = null
-var offset = 0
+var offset = Vector2()
 var releaseTension=false
-onready var links = $Links 
-var direction = Vector2(0,0)
+var tenseLastFrame=false
+
+onready var links = $Links
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-
-#func _process(_delta: float) -> void:
-	
-
 func _physics_process(delta):
 	if shooter == null:
 		queue_free()
@@ -34,27 +31,34 @@ func _physics_process(delta):
 		links.region_rect.size.y = length * 2
 		
 	if isAttached:
+		tenseLastFrame = false
 		if releaseTension:
 			length = max(length, global_position.distance_to(shooter.global_position))
-		else:	
+		else:
 			if global_position.distance_to(shooter.global_position) >= length:
 				var dist = global_position.distance_to(shooter.global_position)
 				var x_dist = shooter.global_position.x - global_position.x
 				var y_dist = shooter.global_position.y - global_position.y
-				var ang = atan2(y_dist, x_dist)
-				shooter.position.x -= (dist-length)*cos(ang)
-				shooter.position.y -= (dist-length)*sin(ang)
-				
 				var mag = sqrt(pow(shooter.motion.x, 2)+pow(shooter.motion.y, 2))
-				var motionAng = atan2(shooter.motion.y, shooter.motion.x)
-				var mag2 = mag*sin(90-ang+atan2(y_dist, x_dist))
-				print(mag)
+				var ang = atan2(y_dist, x_dist)
 				
-				shooter.motion-=Vector2(mag2*cos(ang), mag2*sin(ang))
-				#TODO replace with conserved momentum formula if already at max range
+				if !tenseLastFrame:
+					shooter.position.x -= (dist-length)*cos(ang)
+					shooter.position.y -= (dist-length)*sin(ang)
+					
+					var motionAng = atan2(shooter.motion.y, shooter.motion.x)
+					var mag2 = mag*sin(90-ang+atan2(y_dist, x_dist))
+					
+					shooter.motion-=Vector2(mag2*cos(ang), mag2*sin(ang))
+				else:
+					var newAng = ang - mag/length
+					var newPos = Vector2(mag*cos(newAng), mag*sin(newAng))
+					shooter.motion = newPos - shooter.position
+				tenseLastFrame = true
+
 
 func isTense():
-	return global_position.distance_to(shooter.global_position) >= length*0.99
+	return global_position.distance_to(shooter.global_position) >= length*0.999
 
 func setShooter(shooter):
 	self.shooter = shooter
