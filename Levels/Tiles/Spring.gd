938d6fe684c103e89ec7a8204408c58e9471b74d
CaptainAdmiral <mattb1197@live.co.uk>
2020-07-12 18:32:57 +0100

    Added Springs

---
+++ Levels/Tiles/Spring.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+
+const POWER = 2000
+
+enum DIRECTION { up, down, left, right }
+export(DIRECTION) var direction
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if direction == DIRECTION.up:
+		rotation_degrees = 0
+	elif direction == DIRECTION.down:
+		rotation_degrees = 180
+	elif direction == DIRECTION.left:
+		rotation_degrees = 270
+	elif direction == DIRECTION.right:
+		rotation_degrees = 90
+
+
+func onCollision(body):
+	if body == get_node("/root/World/Player"):
+		
+		
+		if direction == DIRECTION.up:
+			body.position.y -= 64
+			body.motion.y = -POWER
+		elif direction == DIRECTION.down:
+			body.motion.y = POWER
+		elif direction == DIRECTION.left:
+			body.motion.x = -POWER
+		elif direction == DIRECTION.right:
+			body.motion.x = POWER
