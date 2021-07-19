00628b501ed7e86339ddc3313a811096a5ad5310
CaptainAdmiral <mattb1197@live.co.uk>
2021-07-19 15:28:27 +0100

    Hurt state priority bugfix

--- Living/Player/States/PlayerStateHurt.gd
+++ Living/Player/States/PlayerStateHurt.gd
@@ -4,9 +4,11 @@ class_name PlayerStateHurt
 
 func _init(player).(player, "hurt",""):
 	self.duration = 60
-
-func priority():
-	return 5
+	self._priority = 5
+	
+func on_start():
+	.on_start()
+	_priority = 0
 	
 func update():
 	.update()
