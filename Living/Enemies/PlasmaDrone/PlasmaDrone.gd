34554053a462f533081f6452a81557042d60d431
CaptainAdmiral <mattb1197@live.co.uk>
2021-07-19 19:23:15 +0100

    Removed projectiles from plama drone

--- Living/Enemies/PlasmaDrone/PlasmaDrone.gd
+++ Living/Enemies/PlasmaDrone/PlasmaDrone.gd
@@ -1,13 +1,10 @@
 extends Living
 
-onready var PlasmaProjectile = load("res://Projectile/PlasmaProjectile/PlasmaProjectile.tscn")
-
-const RANGE_DISPLAY_DIST = 1000
+const RANGE_DISPLAY_DIST = 1200
 
 var deagro_range = 0
 
 var target : Node2D = null
-var shoot_queue = []
 
 func _ready():
 	set_max_health(2)
@@ -25,7 +22,7 @@ func _physics_process(delta):
 	
 	if !$ChargeUp.active():
 		if $ChargeUp.just_finished:
-			$RangeIndicator.scale = Vector2(2.56, 2.56)
+			$RangeIndicator.scale = Vector2(3.5, 3.5)
 		var dist = RANGE_DISPLAY_DIST
 		var nearestPlayer = Math.get_nearest_to_point(global_position, get_tree().get_nodes_in_group("players"))
 		dist = max(global_position.distance_to(nearestPlayer.global_position), 1)
@@ -34,24 +31,12 @@ func _physics_process(delta):
 			$RangeIndicator.modulate.a = min(1 - abs(dist-500)/(RANGE_DISPLAY_DIST-500), 1)
 			$RangeIndicator.rotation = global_position.angle_to_point(nearestPlayer.global_position)-0.5*PI
 	
-		if !(shoot_queue.empty() or $ShootCooldown.active()):
-			shoot()
 	else:
 		$RangeIndicator.modulate.a = ($ChargeUp.frame+1)/$ChargeUp.activeFrames * 2
 		$RangeIndicator.scale *= 1.05
 		
-	if target != null and !$ShootCooldown.active():
-		shoot_queue.append(target.global_position)
-		$ShootCooldown.start()
 		
 
-func shoot():
-	var direction = global_position.direction_to(shoot_queue[0])
-	shoot_queue.remove(0)
-	
-	var projectile = PlasmaProjectile.instance()
-	projectile.add_to_scene(get_parent(), position, direction*100, self)
-	projectile.properties.knockback = projectile.motion.normalized()*600
 
 func _on_range_entered(body):
 	if target == null:
